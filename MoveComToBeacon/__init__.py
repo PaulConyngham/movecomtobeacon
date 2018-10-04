bdc543a24250315e1d4f4d5027443b8d61999423
paulscon <paul.steven.conyngham@gmail.com>
2018-10-04 17:15:06 +1000

    0.1.7

--- MoveComToBeacon/__init__.py
+++ MoveComToBeacon/__init__.py
@@ -10,6 +10,6 @@ for environment in environments:
     register(
         id='{}-{}'.format(environment[0], environment[1]),
         entry_point='MoveComToBeacon:{}'.format(environment[0]),
-        timestep_limit=1,
+        timestep_limit=200,
         nondeterministic=True,
     )
