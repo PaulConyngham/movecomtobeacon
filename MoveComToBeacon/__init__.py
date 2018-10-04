2450c6820655d88c635138680e5cf7d833209d0a
paulscon <paul.steven.conyngham@gmail.com>
2018-10-04 16:46:38 +1000

    second upload

--- MoveComToBeacon/__init__.py
+++ MoveComToBeacon/__init__.py
@@ -4,12 +4,12 @@ from gym.envs.registration import register
 from .movecomtobeacon import MoveComtoBeacon
 
 
-environments = [['MoveComtoBeacon', 'v0']]
+environments = [['MoveComToBeacon', 'v0']]
 
 for environment in environments:
     register(
         id='{}-{}'.format(environment[0], environment[1]),
-        entry_point='gym_bandits:{}'.format(environment[0]),
+        entry_point='MoveComToBeacon:{}'.format(environment[0]),
         timestep_limit=1,
         nondeterministic=True,
     )
